7c5a5b1285e0da4a5f51adf09b258aae51c0613e
brandonceja <darth1701@hotmail.com>
2019-03-13 22:32:06 -0600

    modified sing-up

--- includes/signup.php
+++ includes/signup.php
@@ -3,17 +3,23 @@
 		include_once 'dbh.php';
 		$first = mysqli_real_escape_string($conn, $_POST['first']);
 		$last = mysqli_real_escape_string($conn, $_POST['last']);
+		$last2 = mysqli_real_escape_string($conn, $_POST['last']);
 		$email = mysqli_real_escape_string($conn, $_POST['email']);
 		$uid = mysqli_real_escape_string($conn, $_POST['username']);
 		$pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+		$date = mysqli_real_escape_string($conn, $_POST['date']);
+		$colon = mysqli_real_escape_string($conn, $_POST['colon']);
+		$calle = mysqli_real_escape_string($conn, $_POST['calle']);
+		$number = mysqli_real_escape_string($conn, $_POST['number']);
 		//Error handlers
 		//Check for empty fields
-		if(empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd)){
+		if(empty($first) || empty($last) || empty($last2) || empty($email) || empty($uid) || empty($pwd) || empty($date) || empty($colon) || 
+			empty($calle) || empty($number)){
 			header("location: ../signup-view.php?signup=empty");
 			exit();
 		}else{
 			//Check if input characters are valid
-			if(!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last)){
+			if(!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last) || !preg_match("/^[a-zA-Z]*$/", $last2)){
 				header("location: ../signup.php?signup=invalid");
 				exit();
 			}else{
@@ -33,17 +39,21 @@
 						//Hashing the password
 						$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
 						//Insert the user in the database
-						$sql = "INSERT INTO users (first, last, username, pass, email) VALUES ('$first', '$last', '$uid', '$hashedPwd', '$email');";
+						$sql = "INSERT INTO users (nombre, apellidoP, apellidoM, username, pass, email, fecha_nac, colonia, calle, numero)
+							 VALUES ('$first', '$last', '$last2', '$uid', '$hashedPwd', '$email', '$date', '$colon', '$calle','$number');";
 						mysqli_query($conn, $sql);
-						//login the user here
 						$_SESSION['u_id'] = $row['id'];
-						$_SESSION['u_first'] = $row['first'];
-						$_SESSION['u_last'] = $row['last'];
-						$_SESSION['u_email'] = $row['email'];
+						$_SESSION['u_nameR'] = $row['nombre'];
+						$_SESSION['u_apat'] = $row['apellidoP'];
+						$_SESSION['u_amat'] = $row['apellidoM'];
 						$_SESSION['u_name'] = $row['username'];
+						$_SESSION['u_email'] = $row['email'];
+						$_SESSION['u_fecha'] = $row['fecha_nac'];
+						$_SESSION['u_colonia'] = $row['colonia'];
+						$_SESSION['u_calle'] = $row['calle'];
+						$_SESSION['u_num'] = $row['numero'];
 
-
-						header("location: ../index.php?login=true");
+						header("location: ../includes/users/profile.php");
 						exit();
 					}
 				}
--- signup-view.php
+++ signup-view.php
@@ -18,26 +18,27 @@
 					<h1><span class="inov"><span class="big">I</span>NOVO</span><span class="med"><span class="big">M</span>ED</span></h1>
 				</div>
 		
-			<form id="signup" action="includes/signup.php" method="POST">
-				<span>Nombre</span>
-				<br>
-				<input type="text" name="first" placeholder="Nombre">
-				<br>
-				<span>Apellidos</span>
-				<br>
-				<input type="text" name="last" placeholder="Apellidos">
-				<br>
-				<span>Nombre de usuario</span>
-				<br>
-				<input type="text" name="username" placeholder="Usuario">
-				<br>
-				<span>Correo electrónico</span>
-				<br>
-				<input type="email" name="email" placeholder="Correo electrónico">
-				<br>
-				<span>Contraseña</span>
-				<br>
-				<input type="password" name="pwd" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;">
+			<form id="signup" action="includes/signup.php" method="POST"><br>
+				<span>Nombre</span><br>
+				<input type="text" name="first" placeholder="Nombre"><br>
+				<span>Apellido Paterno</span>	<br>	
+				<input type="text" name="last" placeholder="Apellido Paterno"><br>
+				<span>Apellido Materno</span>	<br>	
+				<input type="text" name="last2" placeholder="Apellido Materno"><br>
+				<span>Nombre de usuario</span><br>
+				<input type="text" name="username" placeholder="Usuario"><br>
+				<span>Correo electrónico</span><br>
+				<input type="email" name="email" placeholder="Correo electrónico"><br>
+				<span>Contraseña</span><br>
+				<input type="password" name="pwd" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"><br>
+				<span>Fecha de nacimiento</span><br>
+				<input type="date" name="date"><br>
+				<span>Colonia</span><br>
+				<input type="text" name="colon" placeholder="Colonia"><br>
+				<span>Calle</span><br>
+				<input type="text" name="calle" placeholder="Calle"><br>
+				<span>Número</span><br>
+				<input type="number" name="number" placeholder="Número"><br>
 				<br>
 				<button name="submit" type="submit">Siguiente</button>
 			</form>
